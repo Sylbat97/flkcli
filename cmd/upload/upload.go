4dcb113f34962832a0078933b8a9ef45e9b4bc55
Sylbat97 <sylbat@live.be>
2024-04-16 11:55:31 +0200

    add ability to sync current folder with flickr album

--- cmd/upload/upload.go
+++ cmd/upload/upload.go
@@ -4,12 +4,16 @@ import (
 	"flkcli/cmd"
 	"flkcli/flkutils"
 	"fmt"
+	"io/fs"
 	"os"
+	"path/filepath"
 	"strings"
 	"sync"
 
 	"github.com/spf13/cobra"
 	"gopkg.in/masci/flickr.v3"
+	"gopkg.in/masci/flickr.v3/photos"
+	"gopkg.in/masci/flickr.v3/photosets"
 )
 
 type LoadingBar struct {
@@ -21,18 +25,38 @@ type LoadingBar struct {
 	LastLoaded string
 }
 
-// TODO
-// Add a flag to specify the directory to upload photos from if not the current directory
-// Add ability to specify a new set to create and upload photos to
+type PhotoUploadInfo struct {
+	FilePath        string
+	Overwrite       bool
+	PreviousPhotoId string
+}
+
+var syncMode bool
+var overwrite bool
+var delete bool
+var setName string
+var setId string
+var photosInSet []photosets.Photo
 
 var UploadCmd = &cobra.Command{
 	Use:   "upload",
 	Short: "Upload photos to flickr",
 	Long:  `Upload all the photos in the specified directory to flickr`,
 	Run: func(command *cobra.Command, args []string) {
-		sets, _ := command.Flags().GetStringArray("sets")
+		// sets, _ := command.Flags().GetStringArray("sets")
+		syncMode, _ := command.Flags().GetBool("sync")
+		overwrite, _ := command.Flags().GetBool("overwrite")
+		delete, _ := command.Flags().GetBool("delete")
+		if err := validateArgs(args, syncMode, overwrite, delete); err != nil {
+			fmt.Printf("Error: %s", err)
+			return
+		}
+		if len(args) > 0 {
+			setName = args[0]
+		}
+
 		// Initialize the client
-		_, err := cmd.GetFlickrClient()
+		client, err := cmd.GetFlickrClient()
 		if err != nil {
 			fmt.Printf("Error: %s", err)
 			return
@@ -55,7 +79,18 @@ var UploadCmd = &cobra.Command{
 			return
 		}
 
-		var photosToUpload []string
+		if syncMode {
+			setId, err = flkutils.GetSetByName(client, setName, "")
+			if err != nil {
+				fmt.Printf("Error: %s", err)
+				return
+			}
+			if setId != "" {
+				photosInSet, _ = flkutils.GetPhotosInSet(client, setId, "")
+			}
+		}
+
+		var photosToUpload []PhotoUploadInfo
 
 		for _, file := range files {
 			// Check if the file is a photo and ignore it if not
@@ -64,79 +99,171 @@ var UploadCmd = &cobra.Command{
 				fmt.Printf("\nIgnoring: %s\n", file.Name())
 				continue
 			}
+
+			if syncMode {
+				// Check if the photo already exists in the set
+				if result, id := isPhotoInSet(file, photosInSet); result {
+					if overwrite {
+						// Add the file to the list of photos to upload
+						photosToUpload = append(photosToUpload, PhotoUploadInfo{FilePath: file.Name(), Overwrite: true, PreviousPhotoId: id})
+					}
+					continue
+				}
+			}
 			// Add the file to the list of photos to upload
-			photosToUpload = append(photosToUpload, file.Name())
+			photosToUpload = append(photosToUpload, PhotoUploadInfo{FilePath: file.Name(), Overwrite: false, PreviousPhotoId: ""})
 
 		}
 
-		// Initialize the loading bar
-		bar := LoadingBar{Total: len(photosToUpload), Loaded: 0}
-		// Print the number of photos that will be uploaded
-		printLoadingBar(bar)
-		var mutex sync.Mutex
-		var wg sync.WaitGroup
-		maxGoroutines := 20
-		guard := make(chan struct{}, maxGoroutines)
+		if len(photosToUpload) == 0 {
+			fmt.Println("No photos to upload")
+		} else {
+			// Initialize the loading bar
+			bar := LoadingBar{Total: len(photosToUpload), Loaded: 0}
+			// Print the number of photos that will be uploaded
+			printLoadingBar(bar)
 
-		// Iterate over all the photos to upload
-		for _, filePath := range photosToUpload {
-			// Print start of upload with value of i
-			wg.Add(1)
-			guard <- struct{}{}
-			go func(filePath string) {
-				defer wg.Done()
+			var mutex sync.Mutex
+			var wg sync.WaitGroup
+			maxGoroutines := 20
+			guard := make(chan struct{}, maxGoroutines)
 
-				client, err := flkutils.GetFlickrClient()
-				if err != nil {
-					fmt.Printf("Error: %s", err)
-					<-guard
-					return
-				}
-				params := flickr.NewUploadParams()
-				// Restrict the photo to private by default
-				params.IsPublic = false
-				params.IsFamily = false
-				params.IsFriend = false
-				resp, err := flickr.UploadFile(client, filePath, params)
-				if err != nil {
-					fmt.Printf("Error: %s", err)
-					<-guard
-					return
-				}
+			// Iterate over all the photos to upload
+			for _, pui := range photosToUpload {
+				wg.Add(1)
+				guard <- struct{}{}
+				go func(pui PhotoUploadInfo) {
+					defer wg.Done()
 
-				if resp.Status != "ok" {
-					fmt.Printf("Error: %s", resp.Extra)
-					<-guard
-					return
-				}
-
-				// Iterate over all the sets to upload the photo to
-				for _, set := range sets {
-					err := flkutils.AddToPhotoSet(client, resp.ID, set)
+					client, err := flkutils.GetFlickrClient()
 					if err != nil {
 						fmt.Printf("Error: %s", err)
+						<-guard
 					}
-				}
 
-				mutex.Lock()
-				bar.Loaded++
-				bar.LastLoaded = filePath
-				fmt.Printf("\nUploaded: %s\n", filePath)
-				printLoadingBar(bar)
-				mutex.Unlock()
-				<-guard
-			}(filePath)
+					resp, err := uploadPhoto(client, pui)
+					if err != nil {
+						fmt.Printf("Error: %s", err)
+						<-guard
+					}
+
+					// Iterate over all the sets to upload the photo to
+					// for _, set := range sets {
+					// 	err := flkutils.AddToPhotoSet(client, resp.ID, set)
+					// 	if err != nil {
+					// 		fmt.Printf("Error: %s", err)
+					// 	}
+					// }
+
+					mutex.Lock()
+					if setId == "" && syncMode {
+						setId, err = flkutils.CreateSet(client, setName, "", resp.ID)
+						if err != nil {
+							fmt.Printf("Error: %s", err)
+						}
+					} else if syncMode {
+						err := flkutils.AddToPhotoSet(client, resp.ID, setId)
+						if err != nil {
+							fmt.Printf("Error: %s", err)
+						}
+					}
+					bar.Loaded++
+					bar.LastLoaded = pui.FilePath
+					fmt.Printf("\nUploaded: %s\n", pui.FilePath)
+					printLoadingBar(bar)
+					mutex.Unlock()
+					<-guard
+				}(pui)
+			}
+			wg.Wait()
 		}
 
-		wg.Wait()
+		var photosToDelete []string
 
+		if delete {
+			for _, photo := range photosInSet {
+				found := false
+				for _, file := range files {
+					extension := filepath.Ext(file.Name())
+					fileName := strings.TrimSuffix(file.Name(), extension)
+					if strings.EqualFold(strings.ToLower(photo.Title), strings.ToLower(fileName)) {
+						found = true
+						break
+					}
+				}
+				if !found {
+					photosToDelete = append(photosToDelete, photo.Id)
+				}
+			}
+			if len(photosToDelete) > 0 {
+				fmt.Printf("\nDeleting %d photos...\n", len(photosToDelete))
+				// Ask for confirmation
+				fmt.Print("Are you sure you want to delete these photos? (y/n): ")
+				var response string
+				fmt.Scanln(&response)
+				if strings.ToLower(response) == "y" {
+					for _, photoId := range photosToDelete {
+						_, err := photos.Delete(client, photoId)
+						if err != nil {
+							fmt.Printf("Error: %s", err)
+						}
+					}
+				}
+			}
+		}
 	},
+}
+
+func isPhotoInSet(file fs.DirEntry, photosInSet []photosets.Photo) (bool, string) {
+	for _, photo := range photosInSet {
+		extension := filepath.Ext(file.Name())
+		fileName := strings.TrimSuffix(file.Name(), extension)
+		if strings.EqualFold(strings.ToLower(photo.Title), strings.ToLower(fileName)) {
+			// Print that the file already exists in the set
+			return true, photo.Id
+		}
+	}
+	return false, ""
 }
 
 func init() {
 	// Initialize empty string array
-	UploadCmd.PersistentFlags().StringArray("sets", []string{}, "Sets to upload the photos to")
+	//UploadCmd.PersistentFlags().StringArray("sets", []string{}, "Sets to upload the photos to") TODO move to separate command
+	UploadCmd.PersistentFlags().BoolVarP(&syncMode, "sync", "s", false, "Sync mode")
+	UploadCmd.PersistentFlags().BoolVarP(&overwrite, "overwrite", "o", false, "Overwrite already existing photos")
+	UploadCmd.PersistentFlags().BoolVarP(&delete, "delete", "d", false, "Delete distant photos from flickr that are not in the local directory")
+
 	cmd.RootCmd.AddCommand(UploadCmd)
+}
+
+func validateArgs(args []string, syncMode bool, overwrite bool, delete bool) error {
+	if (len(args) == 0 || args[0] == "") && syncMode {
+		return fmt.Errorf("album name is required in sync mode")
+	}
+	if overwrite && delete && !syncMode {
+		return fmt.Errorf("overwrite and delete flags can only be used in sync mode")
+	}
+	return nil
+}
+
+func uploadPhoto(client *flickr.FlickrClient, pui PhotoUploadInfo) (*flickr.UploadResponse, error) {
+	params := flickr.NewUploadParams()
+	// Restrict the photo to private by default
+	params.IsPublic = false
+	params.IsFamily = false
+	params.IsFriend = false
+	resp, err := flickr.UploadFile(client, pui.FilePath, params)
+	if err != nil {
+		return resp, err
+	}
+	if resp.Status != "ok" {
+		fmt.Printf("Error: %s", resp.Extra)
+		return resp, err
+	}
+	if pui.Overwrite {
+		photos.Delete(client, pui.PreviousPhotoId)
+	}
+	return resp, nil
 }
 
 func isPhoto(file os.DirEntry) bool {
--- flkutils/sets.go
+++ flkutils/sets.go
@@ -2,6 +2,7 @@ package flkutils
 
 import (
 	"fmt"
+	"strings"
 
 	"gopkg.in/masci/flickr.v3"
 	"gopkg.in/masci/flickr.v3/photosets"
@@ -40,4 +41,49 @@ func AddToPhotoSet(client *flickr.FlickrClient, photoId, photoSetId string) erro
 	}
 
 	return nil
+}
+
+func GetSetByName(client *flickr.FlickrClient, setName, userid string) (string, error) {
+	_, sets, err := ListSets(client, userid)
+	if err != nil {
+		return "", fmt.Errorf("failed to get list of sets: %w", err)
+	}
+	for _, set := range sets {
+		if strings.EqualFold(strings.ToLower(set.Title), strings.ToLower(setName)) {
+			return set.Id, nil
+		}
+	}
+	return "", nil
+}
+
+func GetPhotosInSet(client *flickr.FlickrClient, setId, userId string) (photosetitems []photosets.Photo, error error) {
+	response, err := photosets.GetPhotos(client, true, setId, userId, 0)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to get list of sets: %w", err)
+	}
+
+	photoItems := response.Photoset.Photos
+
+	for response.Photoset.Page < response.Photoset.Pages {
+		response, err = photosets.GetPhotos(client, true, setId, userId, response.Photoset.Page+1)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get list of sets: %w", err)
+		}
+		photoItems = append(photoItems, response.Photoset.Photos...)
+	}
+	return photoItems, nil
+}
+
+func CreateSet(client *flickr.FlickrClient, title, description, primaryPhotoId string) (string, error) {
+	response, err := photosets.Create(client, title, description, primaryPhotoId)
+	if err != nil {
+		return "", fmt.Errorf("failed to create set: %w", err)
+	}
+
+	if response.Status != "ok" {
+		return "", fmt.Errorf("failed to create set: %s", response.Extra)
+	}
+
+	return response.Set.Id, nil
 }
